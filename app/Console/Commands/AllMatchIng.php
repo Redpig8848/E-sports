a4e9774dc7cee1554290dfcd8ea47f36aaf2cad5
Your Name <you@example.com>
2020-08-12 17:18:37 +0800

    定时

---
+++ app/Console/Commands/AllMatchIng.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Console\Command;
+use Illuminate\Routing\Route;
+
+class AllMatchIng extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:AllMatchIng';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $home = new HomeController();
+        $home->index();
+    }
+}
