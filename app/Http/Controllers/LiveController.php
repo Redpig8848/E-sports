237fc7a3f6f2e2a259e96a1282b47e21bd106625
Your Name <you@example.com>
2020-08-16 14:50:46 +0800

    1

--- app/Http/Controllers/LiveController.php
+++ app/Http/Controllers/LiveController.php
@@ -10,44 +10,43 @@ class LiveController extends Controller
     // 直播页
 
 
-    function Video($id){
-        $matching = DB::table('allmatching')->select('eventsimg','events','BO','tv','team1','team1img',
-            'team1winnum','team2winnum','team2img','team2','now')
-            ->where('id',$id)
+    function Video($id)
+    {
+        $matching = DB::table('allmatching')->select('eventsimg', 'events', 'BO', 'tv', 'team1', 'team1img',
+            'team1winnum', 'team2winnum', 'team2img', 'team2', 'now')
+            ->where('id', $id)
             ->get()
             ->toArray();
 
 
         $matchtime = DB::table('allschedule')->select('matchtime')
-            ->where('team1',$matching[0]->team1)
-            ->where('team2',$matching[0]->team2)
-            ->where('events',$matching[0]->events)
+            ->where('team1', $matching[0]->team1)
+            ->where('team2', $matching[0]->team2)
+            ->where('events', $matching[0]->events)
             ->get()
             ->toArray();
         try {
             $strtime = strtotime($matchtime[0]->matchtime);
-            $date  = date('m月d日 H:i',$strtime);
-        }catch (\Exception $exception){
+            $date = date('m月d日 H:i', $strtime);
+        } catch (\Exception $exception) {
             $date = date('m月d日');
         }
 
         $match = (array)$matching[0];
-        $tv = array_filter(explode('|',$match['tv']));
+        $tv = array_filter(explode('|', $match['tv']));
         $tv_array = array();
-        foreach ($tv as $value){
-            $tv_array = array_add($tv_array,substr($value,0,strpos($value,'=>')),
-                substr($value,strpos($value,'=>')+2));
+        foreach ($tv as $key => $value) {
+            $tv_name = substr($value, 0, strpos($value, '=>'));
+            $tv_link = substr($value, strpos($value, '=>') + 2);
+            $tv_array = array_add($tv_array, $key, array('name' => $tv_name, 'link' => $tv_link));
         }
         $match['tv'] = $tv_array;
-        $match = array_add($match,'time',$date);
+        $match = array_add($match, 'time', $date);
         $array = [$match];
         return $array;
 //        dd(array_add($match,'time',date('m月d日 H:i',$strtime)));
 
     }
-
-
-
 
 
 }
