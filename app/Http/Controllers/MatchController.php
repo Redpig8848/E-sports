f57f9e4fcd2f6a98a281cb91a310c4b9464a1a54
Your Name <you@example.com>
2020-08-16 17:29:57 +0800

    1

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -9,25 +9,26 @@ class MatchController extends Controller
 {
     //
     // 根据ID获取赛事所有比赛
-    public function index($id){
-        $match = DB::table('match')->where('id',$id)
+    public function index($id)
+    {
+        $match = DB::table('match')->where('id', $id)
             ->get()
             ->toArray();
-        $matchs =  DB::table('schedulematch')->where('eventid',$id)
+        $matchs = DB::table('schedulematch')->where('eventid', $id)
             ->get()
             ->toArray();
-
-        return array('title'=>$match,'body'=>$matchs);
-
-
-
+        foreach ($matchs as $key => $v) {
+            if ($v->team1img == 'http://45.157.91.154/static/') {
+                $matchs[$key]->team1img = '';
+            }
+            if ($v->team2img == 'http://45.157.91.154/static/') {
+                $matchs[$key]->team2img = '';
+            }
+        }
+        return array('title' => $match, 'body' => $matchs);
 
 
     }
-
-
-
-
 
 
 }
