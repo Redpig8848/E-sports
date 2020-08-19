c7086685471eb2e0219f083d49bddf4696fdb982
Your Name <you@example.com>
2020-08-19 16:40:12 +0800

    1

--- app/Http/Controllers/Auth/ServiceActionController.php
+++ app/Http/Controllers/Auth/ServiceActionController.php
@@ -12,7 +12,6 @@ class ServiceActionController extends Controller
 
     public function login(Request $request)
     {
-        dd($_SERVER["REMOTE_ADDR"]);
         $is = DB::table('users')->where('phone', $request['phone'])
             ->where('password', $request['password'])
             ->exists();
